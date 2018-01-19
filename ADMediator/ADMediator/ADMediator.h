988dbb90ccd0c9a64a27eb9002a12c551a645bbf
刘乃彬 <nbliu@91jkys.com>
2018-01-19 16:57:02 +0800

    initial

---
+++ ADMediator/ADMediator/ADMediator.h
@@ -0,0 +1,13 @@
+//
+//  ADMediator.h
+//  ADMediator
+//
+//  Created by Andy on 19/01/2018.
+//  Copyright © 2018 Andy. All rights reserved.
+//
+
+#ifndef ADMediator_h
+#define ADMediator_h
+
+
+#endif /* ADMediator_h */
